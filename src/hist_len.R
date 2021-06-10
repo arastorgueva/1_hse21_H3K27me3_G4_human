fd58cbdb8e81cbafd23780c79cf7023c4e86aaca
arastorgueva <rastorgueva.nast@gmail.com>
2021-06-10 08:15:42 +0300

    length histogram
    length histogram

---
+++ src/hist_len.R
@@ -0,0 +1,23 @@
+getwd()
+setwd('C:/Users/admin/Desktop/3-4_module/bioinf_epigen/GitHub/1_hse21_H3K27me3_G4_human/src/')
+
+source('lib.R')
+
+#NAME <- "H3K27me3_K562.ENCFF801AHF.hg19"
+#NAME <- "H3K27me3_K562.ENCFF801AHF.hg38"
+#NAME <- "H3K27me3_K562.ENCFF881ONN.hg19"
+#NAME <- "H3K27me3_K562.ENCFF881ONN.hg38"
+
+
+bed_df <- read.delim(paste0(DATA_DIR, NAME, '.bed'), as.is = TRUE, header = FALSE)
+colnames(bed_df) <- c('chrom', 'start', 'end')
+bed_df$len <- bed_df$end - bed_df$start
+head(bed_df)
+
+
+ggplot(bed_df) +
+  aes(x = len) +
+  geom_histogram() +
+  ggtitle(NAME, subtitle = sprintf('Number of peaks = %s', nrow(bed_df))) +
+  theme_bw()
+ggsave(paste0('len_hist.', NAME, '.pdf'), path = OUT_DIR)
---
+++ src/lib.R
@@ -0,0 +1,7 @@
+library(ggplot2)
+library(dplyr)
+library(tidyr)   # replace_na
+library(tibble)  # column_to_rownames
+
+DATA_DIR <- 'C:/Users/admin/Desktop/3-4_module/bioinf_epigen/GitHub/1_hse21_H3K27me3_G4_human/data/'
+OUT_DIR <- 'C:/Users/admin/Desktop/3-4_module/bioinf_epigen/GitHub/1_hse21_H3K27me3_G4_human/images'
